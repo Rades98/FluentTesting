27ecae992303010dc532147d72084abd3e90a8f2
Radek Řezníček <Radek.Reznicek@alza.cz>
2025-01-10 01:07:08 +0100

    #51

--- docs/sql/extensions.md
+++ docs/sql/extensions.md
@@ -6,7 +6,7 @@ use `RestoreMsSqlDatabasesAsync`.
 To make this whole work, ensure that you are ***not using master*** as db. This can be achieved by defining database
 name in `UseSql` settings as shown bellow:
 
-```csharp
+```cs
 .UseSql(SqlSeed, (configuration, sqlSettings) =>
 {
     configuration.AddConnectionString("Web", sqlSettings.ConnectionString);
@@ -15,3 +15,52 @@ name in `UseSql` settings as shown bellow:
     opts.Database = "TestDb"; //specify name, default is master
 })
 ```
+
+```csharp
+[Fact]
+public async Task SomeUpdatingOrCreatingtestScenario()
+{
+	//Create backup
+	await fixture.BackupMsSqlDatabasesAsync();
+
+	var res = await fixture.Client.PutAsync("sql", someObject);
+
+	res.AssertStatusCode(System.Net.HttpStatusCode.OK);
+
+	//Restore data
+	await fixture.RestoreMsSqlDatabasesAsync();
+}
+
+```
+
+## Obtain data from sql for futher use
+To Get data there are extension on `ITestsFixture` and `IApplicationFactory` named `GetMsSqlObjectAsync` and `GetRawMsSqlObjectAsync`
+
+This one is trying to map data for you from sql response - for mapping purposes is used JSON, 
+so you should reflect naming etc or use native .NET JSON attributes to achieve so.
+
+```csharp
+var obj = await fixture.GetMsSqlObjectAsync<SomeTable, int>("SomeTable", 1, "Id");
+
+obj?.SomeString.Should().Be("MyString");
+```
+
+Some error may occure in serialization proces, co there is even GetRawMsSqlObjectAsync which returns raw data 
+
+!!! warning "Raw string"
+    This extension returns response from SQL server running inside container, so it is just string which needs some work, but as was foretold, some problems in serialization may occure, so to not let you without this functionality this one comes to play
+
+```csharp
+var data = await fixture.GetRawMsSqlObjectAsync("SomeTable", 1, "Id");
+```
+
+<details>
+	<summary>Example of string data representation returned from container</summary>
+```txt
+Id          SomeInt     SomeString                    
+----------- ----------- ------------------------------
+			1           0 kokos                         
+
+(1 rows affected)
+```
+</details>
--- src/FluentTesting.Sql/Extensions/FixtureExtensions.cs
+++ src/FluentTesting.Sql/Extensions/FixtureExtensions.cs
@@ -3,22 +3,51 @@ using FluentTesting.Common.Interfaces;
 
 namespace FluentTesting.Sql.Extensions
 {
-    public static class FixtureExtensions
-    {
-        /// <summary>
-        /// Backup databases - ignores master, so if you want to use such extension, set database name in 
-        /// <see cref="SqlOptions"/> defined in 
-        /// <see cref="SqlExtensions.UseSql(IApplicationFactoryBuilder, string, Action{Microsoft.Extensions.Configuration.ConfigurationBuilder, SqlContainerSettings}, Action{SqlOptions}?)"/>
-        /// </summary>
-        public static Task<ExecResult> BackupMsSqlDatabasesAsync(this ITestFixture fixture)
-            => fixture.ApplicationFactory.BackupMsSqlDatabasesAsync();
+	public static class FixtureExtensions
+	{
+		/// <summary>
+		/// Backup databases - ignores master, so if you want to use such extension, set database name in 
+		/// <see cref="SqlOptions"/> defined in 
+		/// <see cref="SqlExtensions.UseSql(IApplicationFactoryBuilder, string, Action{Microsoft.Extensions.Configuration.ConfigurationBuilder, SqlContainerSettings}, Action{SqlOptions}?)"/>
+		/// </summary>
+		public static Task<ExecResult> BackupMsSqlDatabasesAsync(this ITestFixture fixture)
+			=> fixture.ApplicationFactory.BackupMsSqlDatabasesAsync();
 
-        /// <summary>
-        /// Restore databases - ignores master, so if you want to use such extension, set database name in 
-        /// <see cref="SqlOptions"/> defined in 
-        /// <see cref="SqlExtensions.UseSql(IApplicationFactoryBuilder, string, Action{Microsoft.Extensions.Configuration.ConfigurationBuilder, SqlContainerSettings}, Action{SqlOptions}?)"/>
-        /// </summary>
-        public static Task<ExecResult> RestoreMsSqlDatabasesAsync(this ITestFixture fixture)
-            => fixture.ApplicationFactory.RestoreMsSqlDatabasesAsync();
-    }
+		/// <summary>
+		/// Restore databases - ignores master, so if you want to use such extension, set database name in 
+		/// <see cref="SqlOptions"/> defined in 
+		/// <see cref="SqlExtensions.UseSql(IApplicationFactoryBuilder, string, Action{Microsoft.Extensions.Configuration.ConfigurationBuilder, SqlContainerSettings}, Action{SqlOptions}?)"/>
+		/// </summary>
+		public static Task<ExecResult> RestoreMsSqlDatabasesAsync(this ITestFixture fixture)
+			=> fixture.ApplicationFactory.RestoreMsSqlDatabasesAsync();
+
+		/// <summary>
+		/// Get object from sql - note that there is no mapping mechanism, so result will be mapped to object via JSON deserialization, 
+		/// in case that primitive mapping provided in this package is not working for your scenario, use <see cref="GetRawMsSqlObjectAsync"/>
+		/// and handle mapping mechanism yourself
+		/// </summary>
+		/// <typeparam name="TObject">type of object representing data</typeparam>
+		/// <typeparam name="TIdentifier">type of Identifier</typeparam>
+		/// <param name="tableName">Name of table</param>
+		/// <param name="key">key</param>
+		/// <param name="identifierName">key name - if not provided, logic tableName+Id will be applied</param>
+		/// <returns></returns>
+		public static Task<TObject> GetMsSqlObjectAsync<TObject, TIdentifier>(this ITestFixture fixture, string tableName, TIdentifier key, string? identifierName = null)
+			where TObject : class, new()
+			where TIdentifier : notnull
+			=> fixture.ApplicationFactory.GetMsSqlObjectAsync<TObject, TIdentifier>(tableName, key, identifierName);
+
+		/// <summary>
+		/// Get raw result from sql
+		/// </summary>
+		/// <typeparam name="TIdentifier">type of Identifier</typeparam>
+		/// <param name="tableName">Name of table</param>
+		/// <param name="key">key</param>
+		/// <param name="identifierName">key name - if not provided, logic tableName+Id will be applied</param>
+		/// <returns></returns>
+		public static Task<string> GetRawMsSqlObjectAsync<TIdentifier>(this ITestFixture fixture, string tableName, TIdentifier key, string? identifierName = null)
+			where TIdentifier : notnull
+			=> fixture.ApplicationFactory.GetRawMsSqlObjectAsync<TIdentifier>(tableName, key, identifierName);
+
+	}
 }
